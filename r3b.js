d6ad114048687fb18a30f82185d123ca59b5e64f
dragonxd55 <37715638+dragonxd55@users.noreply.github.com>
2018-05-11 01:39:17 +0200

    Update r3b.js

--- r3b.js
+++ r3b.js
@@ -14,6 +14,17 @@ client.on('ready', function() {
 	console.log(`i am ready ${client.user.username}`);
     client.user.setGame(prefix + 'مساعدة || Moha');
 });
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('Pong!');
+  }
+});
+
 /*
 ////////////////////////\\\\\\\\\\\\\\\\\\\\\\\\\
 ////////////////////////\\\\\\\\\\\\\\\\\\\\\\\\\
