92330b9144c58b991745d7d782f2e2ac02263dca
dragonxd55 <37715638+dragonxd55@users.noreply.github.com>
2018-05-12 13:23:10 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,10 +5,294 @@ var Canvas = require('canvas')
 var jimp = require('jimp')
 var moment = require("moment");
 const fs = require('fs');
+const ytdl = require('ytdl-core');
+const request = require('request');
+const getYoutubeID = require('get-youtube-id');
+const fetchVideoInfo = require('youtube-info');
+const os = require("os");
+
+
+
+
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
+
+
+
+const yt_api_key = "AIzaSyDeoIH0u1e72AtfpwSKKOSy3IPp2UHzqi4";
+const prefix = '!';
+const discord_token = process.env.BOT_TOKEN;
+client.login(discord_token);
+client.on('ready', function() {
+	console.log(`i am ready ${client.user.username}`);
+    client.user.setGame(prefix + 'مساعدة || Moha');
+});
+/*
+////////////////////////\\\\\\\\\\\\\\\\\\\\\\\\\
+////////////////////////\\\\\\\\\\\\\\\\\\\\\\\\\
+////////////////////////\\\\\\\\\\\\\\\\\\\\\\\\\
+////////////////////////\\\\\\\\\\\\\\\\\\\\\\\\\
+*/
+var servers = [];
+var queue = [];
+var guilds = [];
+var queueNames = [];
+var isPlaying = false;
+var dispatcher = null;
+var voiceChannel = null;
+var skipReq = 0;
+var skippers = [];
+var now_playing = [];
+/*
+\\\\\\\\\\\\\\\\\\\\\\\\V/////////////////////////
+\\\\\\\\\\\\\\\\\\\\\\\\V/////////////////////////
+\\\\\\\\\\\\\\\\\\\\\\\\V/////////////////////////
+\\\\\\\\\\\\\\\\\\\\\\\\V/////////////////////////
+*/
+client.on('ready', () => {});
+var download = function(uri, filename, callback) {
+	request.head(uri, function(err, res, body) {
+		console.log('content-type:', res.headers['content-type']);
+		console.log('content-length:', res.headers['content-length']);
+
+		request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
+	});
+};
+
+client.on('message', function(message) {
+	const member = message.member;
+	const mess = message.content.toLowerCase();
+	const args = message.content.split(' ').slice(1).join(' ');
+
+	if (mess.startsWith(prefix + 'play')) {
+	 function play(connection, message) {
+        var server = v.servers[message.guild.id];
+    
+        server.dispatcher = connection.playStream(v.YTDL(server.queue[0], { audioonly: true }));
+        server.queue.shift();
+        server.dispatcher.on("end", function() {
+            if (server.queue[0]) {
+                v.YTDL.getInfo(server.queue[0], function(err, info) {
+                    message.channel.send("Now playing: `" + info.title + " (" + info.length_seconds + " seconds)" + " by " + info.author.name + " with " + info.view_count + " Views `").catch(err => {
+                        message.channel.send("Error: " + err)
+                    })
+                });
+                play(connection, message)
+                server.dispatcher.setVolume(server.volume)
+            } else { 
+            connection.disconnect();
+            message.channel.send("Stopped music and left voice channel.")
+            }
+        });
+    }
+
+    if (message.channel.type == "dm") {
+        message.channel.send("You have to execute this command on a server!")
+        return; }
+    if (v.os.platform == "linux") {
+        message.channel.send("The music feature is due to extreme lags disabled when running on my Raspberry Pi. :confused:")
+        return; }
+    if (!args.slice(0).join(" ")) {
+        message.channel.send("Please provide a valid link.");
+        return; }
+    if (!message.member.voiceChannel) {
+        message.channel.send("Please join a voice channel first.");
+        return; }
+    if (message.guild.voiceConnection && message.member.voiceChannel) if (message.member.voiceChannel.id != message.guild.voiceConnection.channel.id) {
+        message.channel.send("The bot is not in your voice channel!") 
+        return; }
+    if (message.member.voiceChannel.full) {
+        message.channel.send("Your voice channel is full!");
+        return; }
+    if (!message.member.voiceChannel.joinable) {
+        message.channel.send("Missing permission to join your voice channel!");
+        return; }
+    if (!message.member.voiceChannel.speakable) {
+        message.channel.send("Halp! I can't speak!");
+        return; }        
+    if(!v.servers[message.guild.id]) v.servers[message.guild.id] = {
+        queue: [] }
+    
+
+    //search
+    try {
+        if (!message.content.includes("https://www.yout")) {
+
+            var searchword = args.slice(0).join(" ")
+            message.channel.send("Searching for " + searchword + "...");
+            
+            var kind = "video"
+            var searchword = args.slice(0).join(" ")
+            var maxResults = "1"
+            var safeSearch = "none"
+            var key = "AIzaSyBYOgEG_8iYu3XP6DgDqSH_ErCE93egTQQ"
+
+            const { body } = await v.superagent
+            .get('https://www.googleapis.com/youtube/v3/search?part=snippet&q=' + searchword + '&type=' + kind + '&maxResults=' + maxResults + '&safeSearch=' + safeSearch + '&key=' + key)
+
+            if(body.pageInfo.totalResults < 1) {
+                message.channel.send(":x: No results found.")
+                return;
+            }
+
+            var youtubeurlid = body.items[0].id.videoId
+        
+            var urltoplay = "https://www.youtube.com/watch?v=" + youtubeurlid
+
+        } else {
+            var urltoplay = args.slice(0).join(" ")
+            
+        }
+    } catch(err) {
+        console.log("musicplay YouTube API Error: " + err)
+        message.channel.send("YouTube API Error: " + err)
+    }
+
+    var server = v.servers[message.guild.id];
+
+    server.queue.push(urltoplay);
+    if (!message.guild.voiceConnection) message.member.voiceChannel.join().then(function(connection) {
+        play(connection, message)
+        });        
+
+    v.YTDL.getInfo(urltoplay, function(err, info) {
+        message.channel.send("Added to queue: `" + info.title + " (" + info.length_seconds + " seconds)" + " by " + info.author.name + " with " + info.view_count + " Views `").catch(err => {
+            message.channel.send("Error: " + err)
+        })
+    });
+}
+
+	else if (mess.startsWith(prefix + 'pause')) {
+if (message.channel.type == "dm") {
+	message.channel.send(v.randomstring(v.dmerror))
+	return;
+}
+if (!message.guild.voiceConnection) {
+	message.channel.send("The bot is not connected to a voice channel.")
+	return;
+}
+if (!message.member.voiceChannel) {
+	message.channel.send("Please join a voice channel first.");
+	return; }
+if (message.guild.voiceConnection && message.member.voiceChannel) if (message.member.voiceChannel.id != message.guild.voiceConnection.channel.id) {
+	message.channel.send("The bot is not in your voice channel!") 
+	return; }
+	
+var server = v.servers[message.guild.id];
+if (server.dispatcher) server.dispatcher.pause()
+
+message.channel.send("Music paused.")
+
+}
+else if (message.content.startsWith(prefix + 'resume')) {
+if (message.channel.type == "dm") {
+	message.channel.send(v.randomstring(v.dmerror))
+	return;
+}
+if (!message.guild.voiceConnection) {
+	message.channel.send("The bot is not connected to a voice channel.")
+	return;
+}
+if (!message.member.voiceChannel) {
+	message.channel.send("Please join a voice channel first.");
+	return; }
+if (message.guild.voiceConnection && message.member.voiceChannel) if (message.member.voiceChannel.id != message.guild.voiceConnection.channel.id) {
+	message.channel.send("The bot is not in your voice channel!") 
+	return; }
+
+var server = v.servers[message.guild.id];
+if (server.dispatcher) server.dispatcher.resume()
+
+message.channel.send("Music resumed.")
+
+}
+
+else if (message.content.startsWith(prefix + 'skip')) {
+if (message.channel.type == "dm") {
+	message.channel.send(v.randomstring(v.dmerror))
+	return;
+}
+if (!message.guild.voiceConnection) {
+	message.channel.send("The bot is not connected to a voice channel.")
+	return;
+}
+if (!message.member.voiceChannel) {
+	message.channel.send("Please join a voice channel first.");
+	return; }
+if (message.guild.voiceConnection && message.member.voiceChannel) if (message.member.voiceChannel.id != message.guild.voiceConnection.channel.id) {
+	message.channel.send("The bot is not in your voice channel!") 
+	return; }
+
+var server = v.servers[message.guild.id];
+if (server.dispatcher) server.dispatcher.end();
+
+}
+else if (message.content.startsWith(prefix + 'vol')) {
+
+if (message.channel.type == "dm") {
+	message.channel.send(v.randomstring(v.dmerror))
+	return;
+}
+if (!message.guild.voiceConnection) {
+	message.channel.send("The bot is not connected to a voice channel.")
+	return;
+}
+if (!message.member.voiceChannel) {
+	message.channel.send("Please join a voice channel first.");
+	return; }
+if (message.guild.voiceConnection && message.member.voiceChannel) if (message.member.voiceChannel.id != message.guild.voiceConnection.channel.id) {
+	message.channel.send("The bot is not in your voice channel!") 
+	return; }
+
+var volume = args[0]
+
+if (volume === undefined) {
+	message.channel.send("Please provide a number. 1 is default.")
+	return;
+}
+
+var server = v.servers[message.guild.id];
+server.dispatcher.setVolume(volume)
+server.volume = volume
+message.channel.send("Set volume to " + volume)
+
+}
+
+else if (message.content.startsWith(prefix + 'stop')) {
+if (message.channel.type == "dm") {
+	message.channel.send(v.randomstring(v.dmerror))
+	return;
+}
+if (!message.member.voiceChannel) {
+	message.channel.send("Please join a voice channel first.");
+	return; }
+if (message.guild.voiceConnection && message.member.voiceChannel) if (message.member.voiceChannel.id != message.guild.voiceConnection.channel.id) {
+	message.channel.send("The bot is not in your voice channel!") 
+	return; }
+
+var server = v.servers[message.guild.id];
+
+if (!message.member.voiceChannel) message.channel.send("Nothing to stop here. :o")
+if (!message.guild.voiceConnection) message.channel.send("The Bot is not playing anything...");
+
+if (message.member.voiceChannel) {
+	if (message.guild.voiceConnection) {
+		if (message.guild.voiceConnection.channel.id === message.member.voiceChannel.id) {
+//                    message.channel.send("Stopped music and left voice channel."); //Function "play" already sends a message
+			message.guild.voiceConnection.disconnect();
+		} else {
+			message.channel.send("The Bot is not in your voice channel!");
+			return;
+		}
+	}
+}
+
+}
+});
+
+
 
 client.on('message', msg => {
   if (msg.content === 'ping') {
